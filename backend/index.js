908fb8511789f4f288c4a2eb785ca1ab3a848d7f
Guillaume-Cojan <guillaume.cojan@gmail.com>
2021-07-21 12:32:45 +0300

    first commit

---
+++ backend/index.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.listen(port, (err) => {
+    if (err) throw new Error("not working");
+    console.log(`server running on port ${port}`);
+});
