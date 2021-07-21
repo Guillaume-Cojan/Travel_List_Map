40d0606f23459cc54e5fdaed458139e4b06e4f80
Guillaume-Cojan <guillaume.cojan@gmail.com>
2021-07-21 16:32:35 +0300

    create map component

---
+++ client/src/components/Map.js
@@ -0,0 +1,88 @@
+import { useState } from "react";
+import ReactMapGl, { Marker, Popup } from "react-map-gl";
+import Location from "../assets/location.png";
+import Star from "../assets/ratingstar.png";
+
+function Map() {
+    const [viewport, setViewport] = useState({
+        width: "90vw",
+        height: "70vh",
+        latitude: 48,
+        longitude: 20,
+        zoom: 3,
+    });
+
+    return (
+        <div className="Map">
+            <ReactMapGl
+                {...viewport}
+                mapboxApiAccessToken={process.env.REACT_APP_MAPBOX}
+                onViewportChange={(nextViewport) => setViewport(nextViewport)}
+                mapStyle="mapbox://styles/guillaume-cojan/ckrdhq57i2hyn17nyip7dg9ce"
+            >
+                <Marker
+                    latitude={48.0}
+                    longitude={20.0}
+                    offsetLeft={-20}
+                    offsetTop={-10}
+                >
+                    <img
+                        className="location-mark"
+                        src={Location}
+                        alt="location-mark"
+                    />
+                </Marker>
+                <Popup
+                    latitude={48.0}
+                    longitude={20.0}
+                    closeButton={true}
+                    closeOnClick={false}
+                    anchor="left"
+                >
+                    <div className="place-card">
+                        <label>Place</label>
+                        <h4 className="place-name">This is a place</h4>
+                        <label>Review</label>
+                        <p>This is a very very cool place!</p>
+                        <label>Picture</label>
+                        <img className="place-picture" alt="card-visual"></img>
+                        <label>Rating</label>
+                        <div className="rating-stars">
+                            <img
+                                className="rating-star"
+                                src={Star}
+                                alt="rating-star"
+                            />
+                            <img
+                                className="rating-star"
+                                src={Star}
+                                alt="rating-star"
+                            />
+                            <img
+                                className="rating-star"
+                                src={Star}
+                                alt="rating-star"
+                            />
+                            <img
+                                className="rating-star"
+                                src={Star}
+                                alt="rating-star"
+                            />
+                            <img
+                                className="rating-star"
+                                src={Star}
+                                alt="rating-star"
+                            />
+                        </div>
+                        <span className="username">
+                            Created by <b>Me</b>
+                        </span>
+                        <span className="date">1 hour ago</span>
+                    </div>
+                </Popup>
+            </ReactMapGl>
+        </div>
+    );
+}
+
+export default Map;
