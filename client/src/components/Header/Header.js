d09955636dbdc87ec53b044c5a4890b589a56e2a
Guillaume-Cojan <guillaume.cojan@gmail.com>
2021-07-21 15:00:41 +0300

    set up react app structure and components

---
+++ client/src/components/Header/Header.js
@@ -0,0 +1,9 @@
+function Header() {
+    return (
+        <div className="header-container">
+            <div className="header-quote">I'm the quote</div>
+        </div>
+    );
+}
+
+export default Header;
