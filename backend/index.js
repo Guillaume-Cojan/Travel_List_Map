274232ff92516ea3fa6d43c66de61301aa341bd2
Guillaume-Cojan <guillaume.cojan@gmail.com>
2021-07-21 12:57:51 +0300

    setting up backend folder, install dependencies, create routes

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,21 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
+const pool = require("./config");
+
 const port = process.env.PORT || 5000;
 
+const usersRoute = require("./routes/route-users.js");
+const placesRoute = require("./routes/route-places.js");
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(cors());
+
+app.use("/places", placesRoute);
+app.use("/users", usersRoute);
+
 app.listen(port, (err) => {
-    if (err) throw new Error("not working");
-    console.log(`server running on port ${port}`);
+    if (err) throw new Error("Server is not working! :(");
+    console.log(`Server running on port ${port} 🚀`);
 });
